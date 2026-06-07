3e9589b39add5e7af4601a9f35477e8a5fd87d5c
mustafanaser5666-maker <mustafanaser5666@gmail.com>
2026-06-08 00:46:54 +0300

    Wrap App rendering in React.StrictMode

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+
+const root = document.getElementById('root')
+if (root) {
+  ReactDOM.createRoot(root).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  )
+}
